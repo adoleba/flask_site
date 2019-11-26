1a62fec576686f12e9034874ab9c8efcbb643c15
adoleba <ania.doleba@wp.pl>
2019-11-26 17:05:25 +0100

    userprofile view wih current_user username

--- flask_site/users/views.py
+++ flask_site/users/views.py
@@ -1,8 +1,10 @@
 from flask import render_template
+from flask_login import login_required, current_user
 
 from . import users
 
 
 @users.route('/profile')
+@login_required
 def user_profile():
-    return render_template('users/user_profile.html')
+    return render_template('users/user_profile.html', username=current_user.username)

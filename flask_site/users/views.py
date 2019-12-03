32ca2fd594142e96807e7878d4e58349c04847ef
adoleba <ania.doleba@wp.pl>
2019-12-03 16:01:10 +0100

    init template and user_posts view

--- flask_site/users/views.py
+++ flask_site/users/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from flask_login import login_required, current_user
 
+from flask_site.users.models import User
 from . import users
 
 
@@ -8,3 +9,9 @@ from . import users
 @login_required
 def user_profile():
     return render_template('users/user_profile.html', username=current_user.username)
+
+
+@users.route('/<username>')
+def user_posts(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('users/user_posts.html', user=user)

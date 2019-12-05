5e7b05838d09c04248d0f5e996cdcd0e468ea967
adoleba <ania.doleba@wp.pl>
2019-12-05 16:13:37 +0100

    3 latest posts on homepage

--- flask_site/main/views.py
+++ flask_site/main/views.py
@@ -2,6 +2,7 @@ from flask import render_template
 
 from .models import Home
 from flask_site.common.views import PageView
+from ..blog.models import Post
 
 
 class HomePage(PageView):
@@ -9,5 +10,6 @@ class HomePage(PageView):
         ctx = self.get_context_data(**kwargs)
         ctx.update({
             'home': Home.query.filter_by(id=1).first(),
+            'posts': Post.query.order_by(Post.timestamp.desc())[:3],
         })
         return render_template('index.html', **ctx)

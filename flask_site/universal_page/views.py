26de5338cccfa5033352dfb55a0fdc411a6b4815
adoleba <ania.doleba@wp.pl>
2019-12-09 15:28:50 +0100

    templates for blocks in universal_page

--- flask_site/universal_page/views.py
+++ flask_site/universal_page/views.py
@@ -1,10 +1,13 @@
-from flask import render_template
+from flask import render_template, request
 
 from flask_site.common.views import PageView
 from flask_site.universal_page.models import UniversalPage
 
 
 class SamplePage(PageView):
-    def get(self, **kwargs):
+    def get(self, id, **kwargs):
         ctx = self.get_context_data(**kwargs)
-        return render_template('universal_page/universal_page.html', **ctx)
+        ctx.update({
+            'page': UniversalPage.query.get_or_404(id)
+        })
+        return render_template('universal_page/universal_page.html',  **ctx)

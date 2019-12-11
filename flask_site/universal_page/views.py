e4a5fc9e09a1d37e1956298e3d1d789127b238bf
adoleba <ania.doleba@wp.pl>
2019-12-11 12:11:40 +0100

    rename variables in SamplePage view

--- flask_site/universal_page/views.py
+++ flask_site/universal_page/views.py
@@ -12,52 +12,38 @@ class SamplePage(PageView):
         url = ctx['url']
         page = UniversalPage.query.filter_by(url=url).first()
 
-        order_dict = {}
+        blocks_order_dict = {}
 
         if page:
             if page.blockquote:
                 order_BlockQuote = page.blockquote[0].order_BlockQuote
-                if order_BlockQuote:
-                    order_dict['blockquote'] = order_BlockQuote
+                blocks_order_dict['blockquote'] = order_BlockQuote
 
             if page.smallgreyheader:
                 order_SmallGreyHeader = page.smallgreyheader[0].order_SmallGreyHeader
-                if order_SmallGreyHeader:
-                    order_dict['smallgreyheader'] = order_SmallGreyHeader
+                blocks_order_dict['smallgreyheader'] = order_SmallGreyHeader
 
             if page.blockquotewithheader:
                 order_BlockQuoteWithHeader = page.blockquotewithheader[0].order_BlockQuoteWithHeader
-                if order_BlockQuoteWithHeader:
-                    order_dict['blockquotewithheader'] = order_BlockQuoteWithHeader
+                blocks_order_dict['blockquotewithheader'] = order_BlockQuoteWithHeader
 
             if page.faq:
                 order_Faq = page.faq[0].order_Faq
-                if order_Faq:
-                    order_dict['faq'] = order_Faq
+                blocks_order_dict['faq'] = order_Faq
 
             if page.greyheader:
                 order_GreyHeader = page.greyheader[0].order_GreyHeader
-                if order_GreyHeader:
-                    order_dict['greyheader'] = order_GreyHeader
+                blocks_order_dict['greyheader'] = order_GreyHeader
 
             if page.threecolumnswithheaders:
                 order_ThreeColumnsWithHeaders = page.threecolumnswithheaders[0].order_ThreeColumnsWithHeaders
-                if order_ThreeColumnsWithHeaders:
-                    order_dict['threecolumnswithheaders'] = order_ThreeColumnsWithHeaders
+                blocks_order_dict['threecolumnswithheaders'] = order_ThreeColumnsWithHeaders
 
             if page.whiteheaderwithbutton:
                 order_WhiteHeaderWithButton = page.whiteheaderwithbutton[0].order_WhiteHeaderWithButton
-                if order_WhiteHeaderWithButton:
-                    order_dict['whiteheaderwithbutton'] = order_WhiteHeaderWithButton
+                blocks_order_dict['whiteheaderwithbutton'] = order_WhiteHeaderWithButton
 
-        sorted_dict = {k: v for k, v in sorted(order_dict.items(), key=lambda item: item[1])}
+        blocks_sorted_dict = {block_name: block_order for block_name, block_order in sorted(blocks_order_dict.items(), key=lambda item: item[1])}
 
-        return render_template('universal_page/universal_page.html', page=page, sorted_dict=sorted_dict,  **ctx)
-
-
-
-
-
-
-
-
+        return render_template('universal_page/universal_page.html', page=page,
+                               blocks_sorted_dict=blocks_sorted_dict,  **ctx)

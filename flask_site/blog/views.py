619c3886b81cc095ad546174bae027d025a9571e
adoleba <ania.doleba@wp.pl>
2020-01-15 16:13:52 +0100

    init pagination

--- flask_site/blog/views.py
+++ flask_site/blog/views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, request
 
+from flask_site import config
 from flask_site.blog.models import Post
 from flask_site.common.views import PageView
 
@@ -7,8 +8,9 @@ from flask_site.common.views import PageView
 class BlogPage(PageView):
     def get(self, **kwargs):
         ctx = self.get_context_data(**kwargs)
+        page = request.args.get('page', 1, type=int)
         ctx.update({
-            'posts': Post.query.order_by(Post.timestamp.desc()),
+            'posts': Post.query.order_by(Post.timestamp.desc()).paginate(page, config.Config.POSTS_PER_PAGE, False),
         })
         return render_template('blog/blog_page.html', **ctx)
 
@@ -20,3 +22,8 @@ class PostPage(PageView):
             'post': Post.query.get_or_404(id)
         })
         return render_template('blog/post_page.html', **ctx)
+
+
+
+    #next_url = url_for('index', page=posts.next_num) if posts.has_next else None
+    #prev_url = url_for('index', page=posts.prev_num) if posts.has_prev else None

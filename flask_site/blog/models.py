513b1ef34567918c441187db86b6645c1bcdf532
adoleba <ania.doleba@wp.pl>
2020-04-17 11:59:48 +0200

    postgres database

--- flask_site/blog/models.py
+++ flask_site/blog/models.py
@@ -12,7 +12,7 @@ class Post(db.Model):
     body = db.Column(db.Text(), nullable=False)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
 
-    user_name = db.Column(db.Integer, db.ForeignKey('user.username'))
+    user_name = db.Column(db.String, db.ForeignKey('user.username'))
     user = db.relationship("User", back_populates="posts")
 
     @property

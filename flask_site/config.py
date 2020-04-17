0fcdfb3659de56ba9ceccf881f02fe1ff9c60bdf
adoleba <ania.doleba@wp.pl>
2020-04-17 13:47:44 +0200

    SECRET KEY for production and testing config

--- flask_site/config.py
+++ flask_site/config.py
@@ -16,10 +16,12 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 config = {
